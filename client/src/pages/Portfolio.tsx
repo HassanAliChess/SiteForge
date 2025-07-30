82d5ed06add984fa9f9b137ba2f24c65b992eedf
Hassan Ali <hassanalichess@gmail.com>
2025-07-30 20:09:24 +0500

    Revert "Organize expertise into specific areas for a more focused presentation"
    This reverts commit 1a272662c4700cd5a35ba376e5c0818539602fa0.

--- client/src/pages/Portfolio.tsx
+++ client/src/pages/Portfolio.tsx
@@ -1,12 +1,8 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-import FullStackSection from "@/components/FullStackSection";
-import DataScienceSection from "@/components/DataScienceSection";
-import AISection from "@/components/AISection";
-import TradingSection from "@/components/TradingSection";
-import TechSupportSection from "@/components/TechSupportSection";
-import GamingSection from "@/components/GamingSection";
+import Skills from "@/components/Skills";
+import SkillProgress from "@/components/SkillProgress";
 import Badges from "@/components/Badges";
 import SoftSkills from "@/components/SoftSkills";
 import Contact from "@/components/Contact";
@@ -17,13 +13,9 @@ export default function Portfolio() {
       <Navigation />
       <Hero />
       <About />
-      <FullStackSection />
-      <DataScienceSection />
-      <AISection />
-      <TradingSection />
-      <TechSupportSection />
-      <GamingSection />
+      <Skills />
       <Badges />
+      <SkillProgress />
       <SoftSkills />
       <Contact />
       
